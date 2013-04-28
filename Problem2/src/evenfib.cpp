f49d5b6e3e285deafc56956fd2972ebd148e3977
Scott <scott@porky.(none)>
2013-04-28 11:05:36 -0600

    cleaning code.

--- Problem2/src/evenfib.cpp
+++ Problem2/src/evenfib.cpp
@@ -10,7 +10,6 @@ using namespace std;
 
 const double sqrt5 = sqrt(5.0);
 
-
 long i_at_F(double F) {
 	return log((F * sqrt5) + 0.5) / log((1.0 + sqrt5) / 2.0);
 }
@@ -20,13 +19,11 @@ double F_at_i(long i) {
 }
 
 double sum_evens_to_i(long i) {
-	i = i/3.0;
-	return (F_at_i((3 * i) + 3) + F_at_i(3 * i) - 2.0)/4.0;
+	return (F_at_i(i + 3) + F_at_i(i) - 2.0)/4.0;
 }
 
 int main() {
 	int upper_bound = 3999999;
 	printf("%.0f\n", sum_evens_to_i(i_at_F(upper_bound)));
-
 	return 0;
 }
