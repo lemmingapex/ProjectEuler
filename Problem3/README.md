8355355a12400c424168576e74a041bb206e8434
Scott <scott@porky.(none)>
2013-05-13 18:32:25 -0600

    Problem 3.

---
+++ Problem3/README.md
@@ -0,0 +1,24 @@
+[Largest prime factor](http://projecteuler.net/problem=3)
+====================
+Scott Wiedemann
+
+05/13/2013
+
+Compile It
+----------
+cd ./src
+make
+
+
+Run It
+------
+./largeprimefactor \[long to factor\]
+
+
+Thoughts
+--------
+Nothing fancy here.
+
+Run-time
+--------
+Quite dependent on input.  Worse case: O(n), where n is the long to factor.
---
+++ Problem3/src/largeprimefactor.cpp
@@ -0,0 +1,28 @@
+// Scott Wiedemann
+// 05/13/2013
+// evenfib.cpp
+// Largest prime factor
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	if (argc!=2) {
+		printf("Usage: %s [long to factor]\n", argv[0]);
+		return 1;
+	}
+
+	long toFactor = atol(argv[1]);
+	long k = 2;
+	while(k < toFactor) {
+		if(toFactor % k == 0) {
+			toFactor = toFactor / k--;
+		}
+		k++;
+	}
+	printf("Largest prime: %ld\n", toFactor);
+	return 0;
+}
