a0e6e09c09ca928c65fa98f8b3d0c47029970255
Scott Wiedemann <lemmingapex@gmail.com>
2016-10-05 22:51:57 -0600

    Problem 28

--- Problem28/src/com/lemmingapex/projecteuler/numberspiraldiagonals/NumberSpiralDiagonals.java
+++ Problem28/src/com/lemmingapex/projecteuler/numberspiraldiagonals/NumberSpiralDiagonals.java
@@ -11,14 +11,16 @@ import java.math.BigInteger;
  *
  */
 public class NumberSpiralDiagonals {
-	public long solve(BigInteger N) {
-		return 0;
+	public BigInteger solve(BigInteger N) {
+		BigInteger NSquared = N.multiply(N);
+		BigInteger NCubed = NSquared.multiply(N);
+		return ((BigInteger.valueOf(4).multiply(NCubed)).add(BigInteger.valueOf(3).multiply(NSquared)).add(BigInteger.valueOf(8).multiply(N)).subtract(BigInteger.valueOf(9))).divide(BigInteger.valueOf(6));
 	}
 
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.err.println("Incorrect number of arguments.");
-			System.err.println("Usage: ./quadraticprimes.jar N");
+			System.err.println("Usage: ./numberspiraldiagonals.jar N");
 			System.exit(1);
 		}
 		BigInteger N = new BigInteger(args[0]);
