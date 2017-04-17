72c043ba6f02822980dcee59a5701ad78bb23831
Scott Wiedemann <lemmingapex@gmail.com>
2017-04-17 15:44:22 -0600

    Testing ideas realted to pythagorean triples.

--- Problem094/src/com/lemmingapex/projecteuler/almostequilateraltriangle/AlmostEquilateralTriangle.java
+++ Problem094/src/com/lemmingapex/projecteuler/almostequilateraltriangle/AlmostEquilateralTriangle.java
@@ -10,28 +10,76 @@ package com.lemmingapex.projecteuler.almostequilateraltriangle;
  */
 public class AlmostEquilateralTriangle {
 
+	static public long gcd(long a, long b) {
+		long t = a;
+		if(a < b) {
+			a = b;
+			b = t;
+		}
+		while(b != 0) {
+			t = b;
+			b = a % b;
+			a = t;
+		}
+		return a;
+	}
+
 	public long solve(int N) {
 		long perimeterSum = 0;
 
 		length:
-		for(int l=1; ; l++) {
-			for(int k=l-1; k<=l+1; k+=2) {
-				int p = 2*l+k;
-				if(p <= N) {
-					double A = Math.sqrt(4.0*l*l - k*k)*k*0.25;
-					if(A == Math.floor(A)) {
-						System.out.println(l + " " + l + " " + k + " " + A);
-						perimeterSum += p;
+		for(long m=2; ; m++) {
+			long mSquared = m*m;
+			for(long o=-1; o<=1; o+=2) {
+				double nSquared_double = (mSquared - o)/3.0;
+				int nSquared = (int)nSquared_double;
+				double n_double = Math.sqrt(nSquared_double);
+				// TODO: check all multipules of m and n up to N
+
+				// check if n is an integer
+				//System.out.println(mSquared + " " + nSquared);
+				if(n_double == Math.floor(n_double) && mSquared > nSquared) {
+					long n = (long) n_double;
+					// make sure m and n are co-prime
+					if(AlmostEquilateralTriangle.gcd(m, n) == 1) {
+						long l = mSquared + nSquared;
+						long k = 2*(mSquared - nSquared);
+						long p = 2*l+k;
+						if(p <= N) {
+							System.out.println(l + " " + l + " " + k);
+							perimeterSum += p;
+						} else {
+							break length;
+						}
 					}
-				} else {
-					break length;
 				}
 			}
 		}
 
-
 		return perimeterSum;
 	}
+
+	// public long solve(int N) {
+	// 	long perimeterSum = 0;
+	//
+	// 	length:
+	// 	for(int l=1; ; l++) {
+	// 		for(int k=l-1; k<=l+1; k+=2) {
+	// 			int p = 2*l+k;
+	// 			if(p <= N) {
+	// 				double A = Math.sqrt(4.0*l*l - k*k)*k*0.25;
+	// 				if(A == Math.floor(A)) {
+	// 					System.out.println(l + " " + l + " " + k + " " + A);
+	// 					perimeterSum += p;
+	// 				}
+	// 			} else {
+	// 				break length;
+	// 			}
+	// 		}
+	// 	}
+	//
+	// 	return perimeterSum;
+	// }
 
 	public static void main(String[] args) {
 		if (args.length != 1) {
